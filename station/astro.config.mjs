bc91628fbe2fc5a588d2783b73316212908e25f9
Mykal Machon <mykal@tinybox.dev>
2023-03-17 22:15:46 -0700

    feat: adding chart to station site

--- station/astro.config.mjs
+++ station/astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({
     mode: "standalone"
-  })
+  }),
+  integrations: [react()]
 });
---
+++ station/src/components/WeatherChart.tsx
@@ -0,0 +1,48 @@
+import { Chart as ChartJS, Tooltip, CategoryScale, LinearScale, PointElement, LineElement } from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+type WeatherChartProps = {
+  data: Array<any>;
+};
+
+ChartJS.register({
+  Tooltip,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement
+})
+
+const WeatherChart = ({ data }: WeatherChartProps) => {
+  const options = {
+    responsive: true,
+  };
+
+  const labels = data.map((data) => data['created_at']);
+
+  const chartData = {
+    labels,
+    datasets: [
+      {
+        label: 'Temperature (Pressure)',
+        data: data.map((data) => data.temperature_pres),
+        borderColor: 'rgb(119, 101, 240)',
+        tension: 0.1,
+      },
+      {
+        label: 'Temperature (Humidity)',
+        data: data.map((data) => data.temperature_hum),
+        borderColor: 'rgb(240, 101, 157)',
+        tension: 0.1,
+      },
+    ],
+  };
+
+  return (
+    <section style={{ maxHeight: '45vh' }}>
+      <Line data={chartData} options={options} />
+    </section>
+  );
+};
+
+export default WeatherChart;
